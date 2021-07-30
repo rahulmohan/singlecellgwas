9ec8e622ae034f8c31a705796400e18ecd3e138e
rahulmohan <rahul@dashworks.ai>
2021-07-29 21:10:58 -0700

    Legend

--- single_cell_gwas_app.py
+++ single_cell_gwas_app.py
@@ -4,6 +4,7 @@ import plotly.graph_objects as go
 import streamlit as st 
 
 st.title("Single Cell GWAS Explorer")
+st.write("Size of circles indicate effect size, opacity of circles indicate -log(p-value).")
 pairs = pd.read_csv("trait_celltype_pairs.csv")
 app_startup = True
 
